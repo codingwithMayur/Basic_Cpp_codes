54762140e299a69c7224ed8f5fac8b9db975f1ad
codingwithMayur <codingwithmg149@gmail.com>
2024-11-06 16:49:01 +0530

    Print Numbers 1 to N
    Write a program to input an integer N and print all numbers from 1 to N using a while loop.

---
+++ PrintNumbers_1_to_N.cpp
@@ -0,0 +1,18 @@
+// Print Numbers 1 to N
+// Write a program to input an integer N and print all numbers from 1 to N using a while loop.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cout<<"Enter day number: ";
+    cin>>N;
+    int i=1;
+    while(i<N){
+        cout<<i;
+        i++;
+    }
+
+    return 0;
+}
